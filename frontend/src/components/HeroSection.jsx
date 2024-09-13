b762f1455d9085159c27c589dc760781e7a6fe46
unknown <riyarsunny706@gmail.com>
2024-09-13 16:07:53 +0530

    again chages

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -21,7 +21,6 @@ const HeroSection = () => {
                 <span className=' mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-medium'>No. 1 Job Hunt Website</span>
                 <h1 className='text-5xl font-bold'>Search, Apply & <br /> Get Your <span className='text-[#6A38C2]'>Dream Jobs</span></h1>
                 <p className='font-bold text-2xl text-[#a52626] '>Let the right audience know you're available for new opportunities.</p>
-                <p>Sunny changes</p>
 <p className='text-xl'>With the Open To Work feature, you can either privately notify recruiters or openly share with the OnlyJobs community that you're on the lookout for new job opportunities.</p>
                 <div className='flex w-[40%] shadow-lg border border-gray-200 pl-3 rounded-full items-center gap-4 mx-auto translate-y-5 '>
                     <input
